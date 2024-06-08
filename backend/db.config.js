26eba34bc8c2489444c25aced22add445a6c9eaa
Benny Hernandez <bennylopezhernandez0@gmail.com>
2024-06-08 18:03:44 -0400

    Add files via upload

---
+++ backend/db.config.js
@@ -0,0 +1,10 @@
+// Settings for connecting the DB to app
+module.exports = {
+  development: {
+    host: "localhost",
+    username: "root",
+    password: "gewr4hICRiNazet*hIba",
+    database: "gainsgraphs",
+    dialect: "mysql",
+  },
+};
