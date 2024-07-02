03238cf3eb7c645b1b052fa7a6e21f2d16a10f67
Benny Hernandez <bennylopezhernandez0@gmail.com>
2024-07-01 22:41:51 -0400

    Delete routes directory

--- routes/exerciseRoute.js
+++
@@ -1,7 +0,0 @@
-const { authentication } = require("../controllers/authController");
-const { createExercise } = require("../controllers/exerciseController");
-const router = require("express").Router();
-
-router.route("/").post(authentication, createExercise);
-
-module.exports = router;
